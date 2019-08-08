c0a787630517bc3ab65cb6f075992cf95cdafcb7
root <root@AB9EFA7>
2019-08-08 13:52:48 +0000

    some commit

--- src/AbstractUserSession.php
+++ src/AbstractUserSession.php
@@ -1,69 +1,68 @@
 <?php
+
 namespace PhpUserRecognizer;
 
 class AbstractUserSession
 {
-	protected $loggedInUserId = false;
-	protected $loggedInUser;
+    protected $loggedInUserId = false;
+    protected $loggedInUser;
 
-  
-  public function setAuth0($val)
-  {
-    $this->auth0 = $val;
-  }
-  
-  public function getAuth0()
-  {
-    return $this->auth0;
-  }
+    public function setAuth0($val)
+    {
+        $this->auth0 = $val;
+    }
 
-  public function setLoggedInUserId($val)
-  {
-    $this->loggedInUserId = $val;
-  }
-  
-  public function getLoggedInUserId()
-  {
-    return $this->loggedInUserId;
-  }
+    public function getAuth0()
+    {
+        return $this->auth0;
+    }
 
-  public function setLoggedInUser($val)
-  {
-    $this->loggedInUser = $val;
-  }
-  
-  public function getLoggedInUser()
-  {
-    return $this->loggedInUser;
-  }
+    public function login()
+    {
+        $this->getAuth0()->login();
+    }
 
-	public function isUserLoggedIn()
-	{
-		if ($this->getLoggedInUserId() !== false)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
+    public function logout()
+    {
+        $this->getAuth0()->logout();
+    }
 
+    public function setLoggedInUserId($val)
+    {
+        $this->loggedInUserId = $val;
+    }
 
-  public function hasAdminRole()
-  {
-    return false;
-  }
+    public function getLoggedInUserId()
+    {
+        return $this->loggedInUserId;
+    }
 
-  public function isOAuthSession()
-  {
-    return false;
-  }
+    public function setLoggedInUser($val)
+    {
+        $this->loggedInUser = $val;
+    }
 
+    public function getLoggedInUser()
+    {
+        return $this->loggedInUser;
+    }
 
+    public function isUserLoggedIn()
+    {
+        if (false !== $this->getLoggedInUserId()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function hasAdminRole()
+    {
+        return false;
+    }
+
+    public function isOAuthSession()
+    {
+        return false;
+    }
 }
-
-
-
-
-
--- src/UserSessionRecognizer.php
+++ src/UserSessionRecognizer.php
@@ -1,162 +1,139 @@
 <?php
-namespace PhpUserRecognizer;
 
+namespace PhpUserRecognizer;
 
 class UserSessionRecognizer
 {
+    protected $auth0;
+    protected $userRepository;
 
-  protected $auth0;
-  protected $userRepository;
-
-
-  public function __construct($config)
-  {
-    $this->config = $config;
-  }
-  
-  protected function getConfig()
-  {
-    return $this->config;
-  }
-
-  protected function getUserRepository()
-  {
-    if (!$this->userRepository)
+    public function __construct($config)
     {
-      $this->userRepository = new \Speckvisit\Crud\MongoDb\Repository($this->getConfig(), new UserMapper());
-    }
-    
-    return $this->userRepository;
-    
-  }
-  
-  public function setUserRepository($val)
-  {
-    $this->userRepository = $val;
-  }
-
-  public function getUserById($userId)
-  {
-    return $this->getUserRepository()->getById($userId);
-  }
-
-  
-  public function getUserFactory()
-  {
-    return $this->userFactory;
-  }
-
-  public function setUserFactory($val)
-  {
-    $this->userFactory = $val;
-  }
-
-
-  public function recognizeAuthenticatedUser($session)
-  {
-    $auth0 = $this->getAuth0($session);
-    
-    $userSmall = $auth0->getUser();
-
-    if ($userSmall)
-    {
-      $userSession = new Auth0Session();      
-      if ($this->userExistsLocally($userSmall['sub']))
-      {
-        $loggedInUser = $this->getUserRepository()->getOneByAuth0Id($userSmall['sub']);
-        $this->sporadicallyUpdateUserData($auth0->getIdToken(), $loggedInUser);
-      }
-      else 
-      {
-        $loggedInUser = $this->introduceUserLocally($auth0->getIdToken(), $userSmall['sub']);
-      }
-
-      $userSession->setLoggedInUserId($loggedInUser->getId());
-      $userSession->setLoggedInUser( $loggedInUser );
-    }
-    else 
-    {
-      $userSession = new AnonymousUserSession();      
+        $this->config = $config;
     }
 
- 
-    $userSession->setAuth0($auth0);
-    return $userSession;
-  }
-
-
-
-  protected function getAuth0($symfonySession=null)
-  {
-    $config = array(
-        'domain'        => $this->getConfig()['auth0Domain'],
-        'client_id'     => $this->getConfig()['auth0ClientId'],
-        'client_secret' => $this->getConfig()['auth0Secret'],
-        'redirect_uri'  => $this->getConfig()['auth0Callback'],
-        'audience'      => 'https://'.$this->getConfig()['auth0Domain'].'/userinfo',
-        'scope' => 'openid profile',
-        'persist_id_token' => true,
-        'persist_access_token' => true,
-        'persist_refresh_token' => true
-    );
-
-    if ($symfonySession)
+    protected function getConfig()
     {
-      $config['store'] = new SymfonySessionStore($symfonySession);
-    }
-    else 
-    {
-      throw new \Exception("no symfony session given???????");
+        return $this->config;
     }
 
-    $auth0 = new \Auth0\SDK\Auth0($config);
-    return $auth0;    
-
-  }
-
-
-  protected function userExistsLocally($auth0UserId)
-  {
-      try
-      {
-        $loggedInUser = $this->getUserRepository()->getOneByAuth0Id($auth0UserId);
-        return true;
-      }
-      catch (\Speckvisit\Crud\MongoDb\NoMatchException $e)
-      {
-        return false;
-      }
-  }
-
-  
-
-  protected function introduceUserLocally($idToken, $auth0UserId)
-  {
-      $auth0Api = new \Auth0\SDK\Auth0Api($idToken, $this->getConfig()['auth0Domain']);
-      $userData = $auth0Api->users->get($auth0UserId);      
-
-      $newLocalUser = $this->getUserFactory()->create();
-      $this->updateUserWithData($newLocalUser, $userData);
-      return $newLocalUser;
-  }
-
-
-  protected function sporadicallyUpdateUserData($idToken, $user)
-  {
-    if (rand(0,50) === 1)
+    protected function getUserRepository()
     {
-      $auth0Api = new \Auth0\SDK\Auth0Api($idToken, $this->getConfig()['auth0Domain']);
-      $userData = $auth0Api->users->get($user->getAuth0Id());      
+        if (!$this->userRepository) {
+            $this->userRepository = new \Speckvisit\Crud\MongoDb\Repository($this->getConfig(), new UserMapper());
+        }
 
-      $this->updateUserWithData($user, $userData);
+        return $this->userRepository;
     }
-  }
 
-  protected function updateUserWithData($user, $userData)
-  {
-      $user->setAuth0Id($userData['user_id']);
-      $user->profileImage = $userData['picture'];
-      $user->displayName = isset($userData['given_name']) ? $userData['given_name'] : $userData['nickname'];
-      $this->getUserRepository()->merge($user);
-  }
+    public function setUserRepository($val)
+    {
+        $this->userRepository = $val;
+    }
+
+    public function getUserById($userId)
+    {
+        return $this->getUserRepository()->getById($userId);
+    }
+
+    public function getUserFactory()
+    {
+        return $this->userFactory;
+    }
+
+    public function setUserFactory($val)
+    {
+        $this->userFactory = $val;
+    }
+
+    public function recognizeAuthenticatedUser($session)
+    {
+        $auth0 = $this->getAuth0($session);
+
+        $userSmall = $auth0->getUser();
+
+        if ($userSmall) {
+            $userSession = new Auth0Session();
+            if ($this->userExistsLocally($userSmall['sub'])) {
+                $loggedInUser = $this->getUserRepository()->getOneByAuth0Id($userSmall['sub']);
+                $this->sporadicallyUpdateUserData($auth0->getIdToken(), $loggedInUser);
+            } else {
+                $loggedInUser = $this->introduceUserLocally($auth0->getIdToken(), $userSmall['sub']);
+            }
+
+            $userSession->setLoggedInUserId($loggedInUser->getId());
+            $userSession->setLoggedInUser($loggedInUser);
+        } else {
+            $userSession = new AnonymousUserSession();
+        }
+
+        $userSession->setAuth0($auth0);
+
+        return $userSession;
+    }
+
+    protected function getAuth0($symfonySession = null)
+    {
+        $config = array(
+            'domain' => $this->getConfig()['auth0Domain'],
+            'client_id' => $this->getConfig()['auth0ClientId'],
+            'client_secret' => $this->getConfig()['auth0Secret'],
+            'redirect_uri' => $this->getConfig()['auth0Callback'],
+            'audience' => 'https://'.$this->getConfig()['auth0Domain'].'/userinfo',
+            'scope' => 'openid profile',
+            'persist_id_token' => true,
+            'persist_access_token' => true,
+            'persist_refresh_token' => true,
+        );
+
+        if ($symfonySession) {
+            $config['store'] = new SymfonySessionStore($symfonySession);
+        } else {
+            throw new \Exception('no symfony session given???????');
+        }
+
+        $auth0 = new \Auth0\SDK\Auth0($config);
+
+        return $auth0;
+    }
+
+    protected function userExistsLocally($auth0UserId)
+    {
+        try {
+            $loggedInUser = $this->getUserRepository()->getOneByAuth0Id($auth0UserId);
+
+            return true;
+        } catch (\Speckvisit\Crud\MongoDb\NoMatchException $e) {
+            return false;
+        }
+    }
+
+    protected function introduceUserLocally($idToken, $auth0UserId)
+    {
+        $auth0Api = new \Auth0\SDK\Auth0Api($idToken, $this->getConfig()['auth0Domain']);
+        $userData = $auth0Api->users->get($auth0UserId);
+
+        $newLocalUser = $this->getUserFactory()->create();
+        $this->updateUserWithData($newLocalUser, $userData);
+
+        return $newLocalUser;
+    }
+
+    protected function sporadicallyUpdateUserData($idToken, $user)
+    {
+        if (1 === rand(0, 50)) {
+            $auth0Api = new \Auth0\SDK\Auth0Api($idToken, $this->getConfig()['auth0Domain']);
+            $userData = $auth0Api->users->get($user->getAuth0Id());
+
+            $this->updateUserWithData($user, $userData);
+        }
+    }
+
+    protected function updateUserWithData($user, $userData)
+    {
+        $user->setAuth0Id($userData['user_id']);
+        $user->profileImage = $userData['picture'];
+        $user->displayName = isset($userData['given_name']) ? $userData['given_name'] : $userData['nickname'];
+        $this->getUserRepository()->merge($user);
+    }
 }
